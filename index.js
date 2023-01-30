c1dfa0293345b50eb9072b54930e27e738b51024
Nico <nicopasquariello@gmail.com>
2023-01-30 15:02:32 -0700

    cardKeys() is replacing createCards() in DOMContentLoaded event, createCards() has less lines of code now

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // ON LOAD
 addEventListener('DOMContentLoaded', e => {
-    createCards()
+    cardKeys()
     filter()
     reset()
     searchByType()
@@ -8,58 +8,40 @@ addEventListener('DOMContentLoaded', e => {
 
 
 // BASIC PAGE CONTENT... NO FUNCTIONALITIES
-function createCardObj(arr) {
-    const card = {
-        img: arr[0],
-        price: `${arr[1]}`,
-        name: arr[2],
-        brand: arr[3],
-        description: arr[4],
-        link: arr[5],
-        type: arr[6]
-    };
-    return card;
-}
-function cardKeys(fetchUrl) {
+const url = "http://makeup-api.herokuapp.com/api/v1/products.json"
+function cardKeys(fetchUrl = url) {
     return fetch(fetchUrl)
         .then((resp) => resp.json())
-        .then((data) => {
-            let cardObj = [];
-            data.forEach((obj) => {
-                let cardData = [
-                    obj.image_link,
-                    obj.price,
-                    obj.name,
-                    obj.brand,
-                    obj.description,
-                    obj.product_link,
-                    obj.product_type
-                ];
-                cardObj.push(cardData);
-            });
-            return cardObj;
-        });
+        .then((arrOfObj) => {
+            arrOfObj.forEach(obj => {
+                const card = {
+                    img: obj.image_link,
+                    price: `$${obj.price}`,
+                    name: obj.name,
+                    brand: obj.brand,
+                    description: obj.description,
+                    link: obj.product_link,
+                    type: obj.product_type
+                };
+                if (card.price !== "$0.0" && card.price !== undefined) {
+                return createCards(card)
+                }
+            })
+        })
 }
-const url = "http://makeup-api.herokuapp.com/api/v1/products.json";
-function createCards(fetchUrl = url) {
+
+function createCards(card) {
     const photoGal = document.getElementById("photos");
-    cardKeys(fetchUrl).then((resp) => {
-        resp.forEach((dataSet) => {
-            if (dataSet[1] !== "0.0" && dataSet[1] !== undefined) {
-                let obj = createCardObj(dataSet);
-                let cardDiv = document.createElement("div");
-                cardDiv.id = "card";
-                cardDiv.className = "front";
-                cardDiv.innerHTML = `<img src =${obj.img} alt="image link no longer exists">
-              <p>${obj.name}</p>
-              <p>${obj.brand}</p>
-              <p id='desc'>${obj.description}</p>
-              <button id="link"><a href="${obj.link}">Buy Now</a></button>`;
-                photoGal.appendChild(cardDiv);
-            }
-        });
-        flip()
-    });
+        const cardDiv = document.createElement('div')
+        cardDiv.id = 'card'
+        cardDiv.className = 'front'
+        cardDiv.innerHTML = `<img src =${card.img} alt="image link no longer exists">
+        <p>${card.name}</p>
+        <p>${card.brand}</p>
+        <p id='desc'>${card.description}</p>
+        <button id="link"><a href="${card.link}">Buy Now</a></button>`;
+        photoGal.appendChild(cardDiv);
+flip()
 }
 
 //ADDING FUNCTIONALITY TO PAGE
